f628d538d3c67c443cd66a13d9d3c7e187ee2cf1
Hendri Pretorius <pretorh@gmail.com>
2016-03-22 17:27:20 +0200

    add install notes for first part in chapter 6
    linux api headers, man pages, glibc (with glibc's config and timezone)

---
+++ scripts/6/tz-install.sh
@@ -0,0 +1,15 @@
+ZONEINFO=/usr/share/zoneinfo
+
+mkdir -pv $ZONEINFO/{posix,right}
+
+for tz in etcetera southamerica northamerica europe africa antarctica  \
+          asia australasia backward pacificnew systemv; do
+    zic -L /dev/null   -d $ZONEINFO       -y "sh yearistype.sh" ${tz}
+    zic -L /dev/null   -d $ZONEINFO/posix -y "sh yearistype.sh" ${tz}
+    zic -L leapseconds -d $ZONEINFO/right -y "sh yearistype.sh" ${tz}
+done
+
+cp -v zone.tab zone1970.tab iso3166.tab $ZONEINFO
+# "We use New York because POSIX requires the daylight savings time rules to be in accordance with US rule"
+zic -d $ZONEINFO -p America/New_York
+unset ZONEINFO
