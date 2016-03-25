2d7a48409f77882ba9ae66e93aac6b560ab2381a
Hendri Pretorius <pretorh@gmail.com>
2016-03-25 15:41:24 +0200

    add perl to install notes

---
+++ scripts/6/perl-config.sh
@@ -0,0 +1,8 @@
+export BUILD_ZLIB=False
+export BUILD_BZIP2=0
+sh Configure -des -Dprefix=/usr                 \
+                  -Dvendorprefix=/usr           \
+                  -Dman1dir=/usr/share/man/man1 \
+                  -Dman3dir=/usr/share/man/man3 \
+                  -Dpager="/usr/bin/less -isR"  \
+                  -Duseshrplib
