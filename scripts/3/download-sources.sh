a02060ec1a502c7e5de764d8b4788bc199a3c088
Hendri Pretorius <pretorh@gmail.com>
2016-05-15 18:05:06 +0200

    check md5 checksums of source files
    download md5sums and check sources

--- scripts/3/download-sources.sh
+++ scripts/3/download-sources.sh
@@ -5,3 +5,6 @@ wget http://www.linuxfromscratch.org/lfs/downloads/$VERSION/wget-list
 wget --input-file=wget-list --continue
 wget --continue http://www.linuxfromscratch.org/patches/lfs/systemd/systemd-229-compat-1.patch
 wget --continue http://anduin.linuxfromscratch.org/sources/other/systemd/systemd-229.tar.xz
+
+wget http://www.linuxfromscratch.org/lfs/downloads/$VERSION/md5sums
+md5sum -c md5sums
