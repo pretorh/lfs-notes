7eb91724848fa16d5be0bf618b719e9c47a62429
Hendri Pretorius <pretorh@gmail.com>
2016-07-09 12:45:37 +0200

    download and install gcc 5.3.0 in arch host setup script

--- scripts/0/pacman.sh
+++ scripts/0/pacman.sh
@@ -1,3 +1,12 @@
 # arch: ensure the system is up to date, and install required packages
 pacman -Syu --noconfirm
 pacman -S --noconfirm --needed bash binutils bison bzip2 coreutils diffutils findutils gawk gcc glibc grep gzip m4 make patch perl sed tar texinfo xz
+
+# download gcc-5.3.0 and install it
+wget --continue \
+    -P /var/cache/pacman/pkg/ \
+    https://archive.archlinux.org/packages/g/gcc/gcc-5.3.0-5-x86_64.pkg.tar.xz
+wget --continue \
+    -P /var/cache/pacman/pkg/ \
+    https://archive.archlinux.org/packages/g/gcc-libs/gcc-libs-5.3.0-5-x86_64.pkg.tar.xz
+pacman -U /var/cache/pacman/pkg/gcc-libs-5.3.0-5-x86_64.pkg.tar.xz /var/cache/pacman/pkg/gcc-5.3.0-5-x86_64.pkg.tar.xz
