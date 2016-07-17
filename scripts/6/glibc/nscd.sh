4a170692918ee1787c69448c74d538599d63e616
Hendri Pretorius <pretorh@gmail.com>
2016-07-17 18:45:41 +0200

    add systemd support files for nscd as part of glibc

--- scripts/6/glibc/nscd.sh
+++ scripts/6/glibc/nscd.sh
@@ -1,2 +1,4 @@
 cp -v ../nscd/nscd.conf /etc/nscd.conf
 mkdir -pv /var/cache/nscd
+install -v -Dm644 ../nscd/nscd.tmpfiles /usr/lib/tmpfiles.d/nscd.conf
+install -v -Dm644 ../nscd/nscd.service /lib/systemd/system/nscd.service
