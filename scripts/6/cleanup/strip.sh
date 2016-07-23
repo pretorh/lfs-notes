2b4f3a26f1c92e6dc990eae11c6a572e3d9f2d58
Hendri Pretorius <pretorh@gmail.com>
2016-07-23 15:02:36 +0200

    logout and chroot before stipping

--- scripts/6/cleanup/strip.sh
+++ scripts/6/cleanup/strip.sh
@@ -1,9 +1,19 @@
+# logout of chroot and enter again
+logout
+
+chroot $LFS /tools/bin/env -i            \
+    HOME=/root TERM=$TERM PS1='\u:\w\$ ' \
+    PATH=/bin:/usr/bin:/sbin:/usr/sbin   \
+    /tools/bin/bash --login
+
+# strip
 /tools/bin/find /{,usr/}{bin,lib,sbin} -type f      \
     -exec /tools/bin/strip --strip-debug '{}' ';'   \
     2>&1 \
     | grep -v "File format not recognized" \
     | grep -v "is not an ordinary file"
 
+# cleanup
 rm -rf /tmp/*
 
 rm -f /usr/lib/lib{bfd,opcodes}.a
