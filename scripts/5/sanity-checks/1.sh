7f256d83ccb5da0c9a84104317a4e235bb228f48
Hendri Pretorius <pretorh@gmail.com>
2018-07-30 22:23:54 +0200

    assume 64 bit, use set -e in 1st sanity script

--- scripts/5/sanity-checks/1.sh
+++ scripts/5/sanity-checks/1.sh
@@ -1,4 +1,7 @@
+set -e
+
 echo 'int main(){}' > dummy.c
 $LFS_TGT-gcc dummy.c
-(readelf -l a.out | grep 'Requesting program interpreter: /tools/lib.*/ld-linux-x86.*.so.2' && echo "SUCCESS") || (echo "ERROR!" && exit 1)
+readelf -l a.out | grep 'Requesting program interpreter: /tools/lib64/ld-linux-x86-64.so.2' && echo "SUCCESS" \
+  || (echo "ERROR!" && false)
 rm -v dummy.c a.out
