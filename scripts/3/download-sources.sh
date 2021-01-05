123c2d17efe40de4cee21aaee61cdd22db48f000
Hendri Pretorius <hendri@pretorh.xyz>
2021-01-05 07:27:48 +0200

    setup: pass version into download script
    and remove previous version's files

--- scripts/3/download-sources.sh
+++ scripts/3/download-sources.sh
@@ -1,10 +1,9 @@
-cd $LFS/sources
-export VERSION=xxxxxx
+#!/usr/bin/env sh
 
-wget http://www.linuxfromscratch.org/lfs/downloads/$VERSION/wget-list
+VERSION=${1:-lfs version not specified}
+
+wget "http://www.linuxfromscratch.org/lfs/downloads/$VERSION/wget-list"
 wget --input-file=wget-list --continue
-wget --continue http://www.linuxfromscratch.org/patches/lfs/systemd/systemd-229-compat-1.patch
-wget --continue http://anduin.linuxfromscratch.org/sources/other/systemd/systemd-229.tar.xz
 
-wget http://www.linuxfromscratch.org/lfs/downloads/$VERSION/md5sums
+wget "http://www.linuxfromscratch.org/lfs/downloads/$VERSION/md5sums"
 md5sum -c md5sums
