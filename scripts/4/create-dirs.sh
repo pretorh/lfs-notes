9e184da4c3367e8f33184566a15ab47b0a511925
Hendri Pretorius <hendri@pretorh.xyz>
2021-01-05 10:03:49 +0200

    setup: add script for creating root directory layout

---
+++ scripts/4/create-dirs.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+
+[ -z "$LFS" ] && echo "LFS env var is not set!" && exit 1
+
+# create limited directory layout, tool chain directory
+mkdir -pv "$LFS/"{bin,etc,lib,lib64,sbin,usr,var}
+mkdir -pv "$LFS/tools"
+
+# make lfs user the owner of these
+chown -v lfs "$LFS/"{usr,lib,var,etc,bin,sbin,tools}
