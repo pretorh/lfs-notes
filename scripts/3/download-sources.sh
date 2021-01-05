5d8769ee685508889421efb4754cd70e6a8f098c
Hendri Pretorius <hendri@pretorh.xyz>
2021-01-05 07:32:28 +0200

    setup: overwrite wget-list, md5sums file
    do not download with alternative names

--- scripts/3/download-sources.sh
+++ scripts/3/download-sources.sh
@@ -2,8 +2,8 @@
 
 VERSION=${1:-lfs version not specified}
 
-wget "http://www.linuxfromscratch.org/lfs/downloads/$VERSION/wget-list"
+wget "http://www.linuxfromscratch.org/lfs/downloads/$VERSION/wget-list" -O wget-list
 wget --input-file=wget-list --continue
 
-wget "http://www.linuxfromscratch.org/lfs/downloads/$VERSION/md5sums"
+wget "http://www.linuxfromscratch.org/lfs/downloads/$VERSION/md5sums" -O md5sums
 md5sum -c md5sums
