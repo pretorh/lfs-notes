11839ddac77addebfd64e31261023e3ccf1bf0dc
Hendri Pretorius <hendri@pretorh.xyz>
2021-01-07 12:03:52 +0200

    main: update/fix shellcheck errors in bzip2 scripts

--- scripts/6/3/bzip2-patch.sh
+++ scripts/6/3/bzip2-patch.sh
@@ -1,3 +1,6 @@
-patch -Np1 -i ../bzip2-1.0.6-install_docs-1.patch
+#!/usr/bin/env sh
+# shellcheck disable=SC2016
+
+patch -Np1 -i ../bzip2-*-install_docs-1.patch
 sed -i 's@\(ln -s -f \)$(PREFIX)/bin/@\1@' Makefile
 sed -i "s@(PREFIX)/man@(PREFIX)/share/man@g" Makefile
