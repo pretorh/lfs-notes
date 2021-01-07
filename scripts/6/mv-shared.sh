30a729d1ed1de175f2e949c7de7edbd08a0875ae
Hendri Pretorius <hendri@pretorh.xyz>
2021-01-07 11:38:29 +0200

    main: fix shellcheck warnings in mv-shared

--- scripts/6/mv-shared.sh
+++ scripts/6/mv-shared.sh
@@ -1,5 +1,7 @@
+#!/usr/bin/env sh
+
 NAME=${1?'need name of so file to move'}
 MAIN=$DESTDIR$NAME
 
-mv -v $MAIN.* $DESTDIR/lib
-ln -sfv ../../lib/$(readlink $MAIN) $MAIN
+mv -v "$MAIN".* "$DESTDIR/lib"
+ln -sfv ../../lib/"$(readlink "$MAIN")" "$MAIN"
