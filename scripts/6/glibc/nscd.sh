a9c196ec80f9b0fb17dfa7aa1ffbcce26127b0d9
Hendri Pretorius <hendri@pretorh.xyz>
2021-01-07 10:34:17 +0200

    main: glibc: create single install.sh script
    merge prep, install and nscd into a single script

--- scripts/6/glibc/nscd.sh
+++
@@ -1,4 +0,0 @@
-cp -v ../nscd/nscd.conf $DESTDIR/etc/nscd.conf
-mkdir -pv $DESTDIR/var/cache/nscd
-install -v -Dm644 ../nscd/nscd.tmpfiles $DESTDIR/usr/lib/tmpfiles.d/nscd.conf
-install -v -Dm644 ../nscd/nscd.service $DESTDIR/lib/systemd/system/nscd.service
