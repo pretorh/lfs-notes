769182282d0be4dc3e2b1f5c368b41a0fe6ae229
Hendri Pretorius <hendri@pretorh.xyz>
2021-01-11 15:04:57 +0200

    config: fix locale helper script
    change to only select the locale string, fix selection based on charmap,
    fix shellcheck warnings

--- scripts/7/select-locale.sh
+++ scripts/7/select-locale.sh
@@ -1,12 +1,14 @@
+#!/usr/bin/env bash
+
 set -e
 
 echo "enter ll (language 2 letter)"
-read locale_ll
+read -r locale_ll
 echo "enter CC (coutry 2 letter)"
-read locale_cc
+read -r locale_cc
 
-locale_llcc=$(locale -a | grep -i $locale_ll | grep -i $locale_cc | sort | head -n 1)
-locale_name=$(locale -a | grep $locale_llcc | grep utf8)
+locale_llcc=$(locale -a | grep -i "$locale_ll" | grep -i "$locale_cc" | sort | head -n 1 | awk -F . '{print $1}')
+locale_name=$(locale -a | grep "$locale_llcc" | grep utf8)
 echo "selected $locale_name for $locale_llcc"
 
 locale_charmap=$(LC_ALL=$locale_name locale charmap)
@@ -15,19 +17,11 @@ echo "found charmap: $locale_charmap"
 locale_final="${locale_llcc}.${locale_charmap}"
 echo "locale string: $locale_final"
 
-echo "testing..."
+echo "testing... (language, charmap, currency, international telephone prefix)"
 LC_ALL=$locale_final locale language
 LC_ALL=$locale_final locale charmap
 LC_ALL=$locale_final locale int_curr_symbol
 LC_ALL=$locale_final locale int_prefix
+echo ""
 
-echo "setting"
-echo "LANG=$locale_final" > /etc/locale.conf
-cat /etc/locale.conf
-
-unset locale_ll
-unset locale_cc
-unset locale_llcc
-unset locale_name
-unset locale_charmap
-unset locale_final
+echo "persist by saving 'LANG=$locale_final' to '/etc/locale.conf'"
