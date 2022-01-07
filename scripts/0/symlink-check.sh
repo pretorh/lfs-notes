c9023236d89a263bdf0c14e65b99ff3c602c0ebd
Hendri Pretorius <hendri@pretorh.xyz>
2022-01-07 10:59:50 +0200

    setup: print versions of symlinked yacc, awk, print g++ version

--- scripts/0/symlink-check.sh
+++ scripts/0/symlink-check.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
 echo "/bin/sh -> $(readlink -f /bin/sh)"
 
 if [ -h /usr/bin/yacc ] ; then
     echo "/usr/bin/yacc -> $(readlink -f /usr/bin/yacc)";
+    echo "  which is $(/usr/bin/yacc --version | head -n1)"
 elif [ -x /usr/bin/yacc ] ; then
     echo "yacc is $(/usr/bin/yacc --version | head -n1)"
 else
@@ -12,16 +13,18 @@ fi
 
 if [ -h /usr/bin/awk ]; then
     echo "/usr/bin/awk -> $(readlink -f /usr/bin/awk)";
+    echo "  which is $(/usr/bin/awk --version | head -n1)"
 elif [ -x /usr/bin/awk ]; then
     echo "awk is $(/usr/bin/awk --version | head -n1)"
 else
     echo "awk not found"
 fi
 
+g++ --version | head -n1
 echo 'int main(){}' > dummy.c && g++ -o dummy dummy.c
 if [ -x dummy ] ; then
-    echo "g++ compilation OK";
+    echo "  compilation OK";
 else
-    echo "g++ compilation failed"
+    echo "  compilation failed"
 fi
 rm -f dummy.c dummy
