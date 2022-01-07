8367850e930f790c3a6efb0b8813ece2fa7edfb8
Hendri Pretorius <hendri@pretorh.xyz>
2022-01-07 12:30:57 +0200

    download: also prune md5sums when downloading

--- scripts/3/download-sources.sh
+++ scripts/3/download-sources.sh
@@ -4,6 +4,7 @@ ROOT_URL=https://www.linuxfromscratch.org
 VERSION=${1:?lfs version not specified}
 
 wget "$ROOT_URL/lfs/downloads/$VERSION/wget-list" -O wget-list
+wget "$ROOT_URL/lfs/downloads/$VERSION/md5sums" -O md5sums
 
 # remove sysvinit (on systemd versions), vim and kernel (manually download latest items)
 # replace root urls
@@ -14,12 +15,19 @@ grep wget-list -v \
     sed -s "s|http://www.linuxfromscratch.org|$ROOT_URL|" \
     > wget-list.cleaned
 
+grep md5sums -v \
+    -e sysvinit \
+    -e ' linux-5.' \
+    -e vim- \
+    > md5sums.cleaned
+
 echo "file list changes:"
+echo "wget-list:"
 diff wget-list wget-list.cleaned
+echo "md5sums:"
+diff md5sums md5sums.cleaned
 echo -e "enter to continue"
 read -r
 
 wget --input-file=wget-list.cleaned --continue
-
-wget "$ROOT_URL/lfs/downloads/$VERSION/md5sums" -O md5sums
 md5sum -c md5sums
