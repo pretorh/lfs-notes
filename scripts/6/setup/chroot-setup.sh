c6794ee3684762cc114f42e35b67faae663e10b6
Hendri Pretorius <hendri@pretorh.xyz>
2022-01-09 07:19:24 +0200

    chroot: update files and symlink setup script

--- scripts/6/setup/chroot-setup.sh
+++ scripts/6/setup/chroot-setup.sh
@@ -2,7 +2,10 @@
 
 ln -sv /proc/self/mounts /etc/mtab
 # TODO: why not set the hostname here directly
-echo "127.0.0.1 localhost $(hostname)" > /etc/hosts
+cat > /etc/hosts << EOF
+127.0.0.1 localhost $(hostname)
+::1 localhost
+EOF
 
 # setup users and groups
 
@@ -10,7 +13,7 @@ cat > /etc/passwd << "EOF"
 root:x:0:0:root:/root:/bin/bash
 bin:x:1:1:bin:/dev/null:/bin/false
 daemon:x:6:6:Daemon User:/dev/null:/bin/false
-messagebus:x:18:18:D-Bus Message Daemon User:/var/run/dbus:/bin/false
+messagebus:x:18:18:D-Bus Message Daemon User:/run/dbus:/bin/false
 systemd-bus-proxy:x:72:72:systemd Bus Proxy:/:/bin/false
 systemd-journal-gateway:x:73:73:systemd Journal Gateway:/:/bin/false
 systemd-journal-remote:x:74:74:systemd Journal Remote:/:/bin/false
@@ -19,6 +22,8 @@ systemd-network:x:76:76:systemd Network Management:/:/bin/false
 systemd-resolve:x:77:77:systemd Resolver:/:/bin/false
 systemd-timesync:x:78:78:systemd Time Synchronization:/:/bin/false
 systemd-coredump:x:79:79:systemd Core Dumper:/:/bin/false
+uuidd:x:80:80:UUID Generation Daemon User:/dev/null:/bin/false
+systemd-oom:x:81:81:systemd Out Of Memory Daemon:/:/bin/false
 nobody:x:99:99:Unprivileged User:/dev/null:/bin/false
 EOF
 
@@ -53,6 +58,8 @@ systemd-network:x:76:
 systemd-resolve:x:77:
 systemd-timesync:x:78:
 systemd-coredump:x:79:
+uuidd:x:80:
+systemd-oom:x:81:81:
 wheel:x:97:
 nogroup:x:99:
 users:x:999:
@@ -60,8 +67,7 @@ EOF
 
 # tester user needed in chapter 8
 # TODO: do we need to remove it later? maybe give it a better name and keep it?
-# shellcheck disable=SC2012
-echo "tester:x:$(ls -n "$(tty)" | cut -d" " -f3):101::/home/tester:/bin/bash" >> /etc/passwd
+echo "tester:x:101:101::/home/tester:/bin/bash" >> /etc/passwd
 echo "tester:x:101:" >> /etc/group
 install -o tester -d /home/tester
 
@@ -72,6 +78,5 @@ chmod -v 664  /var/log/lastlog
 chmod -v 600  /var/log/btmp
 
 # done
-
 echo "username and groupname resolution will now work"
 echo "exec /bin/bash --login +h"
