597edcbd18cc52ebcc8c612b6e29db775d96c435
Hendri Pretorius <hendri@pretorh.xyz>
2022-01-10 17:46:32 +0200

    main: change gcc test runner script output
    remove verbosity on chown (noisy) and print the failed test items (not
    just the summaries)

--- scripts/6/gcc/test.sh
+++ scripts/6/gcc/test.sh
@@ -2,13 +2,14 @@
 
 echo "setup testing"
 ulimit -s 32768
-chown -Rv tester .
+chown -R tester .
 
 echo "starting tests"
 time su tester -c "PATH=$PATH make --jobs 4 -k check || echo 'make check failed'"
 
-echo "summary"
-../contrib/test_summary | grep -A7 Summ
+echo "failed tests:"
+../contrib/test_summary | grep FAIL | sort
+echo "# end of FAILed test list"
 
 echo "cleanup permissions"
-chown -Rv root .
+chown -R root .
