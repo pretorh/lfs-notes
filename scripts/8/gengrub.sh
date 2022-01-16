de46e408eb67d812ec3808d0381fd130920c67e8
Hendri Pretorius <hendri@pretorh.xyz>
2022-01-16 10:18:03 +0200

    linux: use uuids in grub, change label
    get the root filesystem's uuid, and use that in root=
    change label to LFS: name, excluding the vmlinuz

--- scripts/8/gengrub.sh
+++ scripts/8/gengrub.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-ROOT="$(mount | grep 'on / ' | awk -F' ' '{print $1}')"
+ROOT="$(findmnt / -o UUID | tail -n1)"
 
 echo "#!/bin/sh"
 echo "exec tail -n +3 \$0"
@@ -10,9 +10,10 @@ echo "# begin of generated menuentries for lfs named kernels"
 while IFS= read -r -d '' file
 do
     linux=${file//\/boot\/}
-    echo "menuentry \"$linux\" {"
-    echo "    # replace $ROOT with UUID=..."
-    echo "    linux $linux root=$ROOT ro"
+    label="LFS: ${linux:8}"
+
+    echo "menuentry \"$label\" {"
+    echo "    linux $linux root=UUID=$ROOT ro"
     echo "}"
 done <   <(find /boot -type f -name 'vmlinuz*lfs*' -print0)
 
