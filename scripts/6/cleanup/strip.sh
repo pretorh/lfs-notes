2e3c7f820d992b082fcfb7a197b4a7c402807b74
Hendri Pretorius <hendri@pretorh.xyz>
2022-02-05 20:30:43 +0200

    cleanup: remove old unreferenced scripts

--- scripts/6/cleanup/strip.sh
+++
@@ -1,6 +0,0 @@
-# strip
-/tools/bin/find /{,usr/}{bin,lib,sbin} -type f      \
-    -exec /tools/bin/strip --strip-debug '{}' ';'   \
-    2>&1 \
-    | grep -v "File format not recognized" \
-    | grep -v "is not an ordinary file"
