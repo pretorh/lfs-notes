8aa12f096b9dc1576cdc77ce01dfde65daa6cc0c
Hendri Pretorius <pretorh@gmail.com>
2023-01-04 18:32:31 +0200

    final: write release info to LFS dir if defined
    to prevent accidentally writing to the host's files

--- scripts/config/os-details.sh
+++ scripts/config/os-details.sh
@@ -2,14 +2,14 @@
 
 version=$(date --iso-8601=minutes)
 
-cat > /etc/os-release << EOF
+cat > "$LFS"/etc/os-release << EOF
 NAME="Linux From Scratch"
 VERSION="$version"
 ID=lfs
 PRETTY_NAME="Linux From Scratch $version"
 EOF
 
-cat > /etc/lsb-release << EOF
+cat > "$LFS"/etc/lsb-release << EOF
 DISTRIB_ID="Linux From Scratch"
 DISTRIB_RELEASE="$version"
 DISTRIB_DESCRIPTION="Linux From Scratch"
