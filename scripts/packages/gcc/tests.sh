66bc6306bdddd503feb7e0993bd7cd3959e645a0
Hendri Pretorius <pretorh@gmail.com>
2023-02-06 17:25:19 +0200

    gcc: remove force-fail in test script
    check for and ignore known failing tests. if there are no other,
    continue, else exit

--- scripts/packages/gcc/tests.sh
+++ scripts/packages/gcc/tests.sh
@@ -8,11 +8,15 @@ echo "starting tests"
 time su tester -c "PATH=$PATH make --jobs 8 -k check || echo 'make check failed'"
 
 echo "failed tests:"
-../contrib/test_summary | grep -E '(XPASS|FAIL)' | sort
+../contrib/test_summary | grep -E '(XPASS|FAIL)' | sort | tee failed-test-summary.log
 echo "# end of FAILed test list"
 
-echo "forcing a break in test script"
-exit 1
+if grep -v "c-c++-common/goacc/kernels-decompose-pr100400-1-2.c" failed-test-summary.log >/dev/null ; then
+  echo "FAIL: there are unexpected failing tests" >&2
+  exit 1
+else
+  echo "only expected failing tests, will continue"
+fi
 
 echo "cleanup permissions"
 chown -R root .
