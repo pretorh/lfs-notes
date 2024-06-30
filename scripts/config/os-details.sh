1bdd2a350afaf0625102e05cb1c425190ecd515c
Hendri Pretorius <pretorh@gmail.com>
2024-06-30 14:32:55 +0200

    config: change os-details to run from inside chroot

--- scripts/config/os-details.sh
+++ scripts/config/os-details.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 
-[ -z "$LFS" ] && echo "LFS env var is not set!" && exit 1
-
 version=$(date --iso-8601=minutes)
 
-cat > "$LFS"/etc/os-release << EOF
+cat > "$DESTDIR"/etc/os-release << EOF
 NAME="Linux From Scratch"
 VERSION="$version"
 ID=lfs
 PRETTY_NAME="Linux From Scratch $version"
 EOF
 
-cat > "$LFS"/etc/lsb-release << EOF
+cat > "$DESTDIR"/etc/lsb-release << EOF
 DISTRIB_ID="Linux From Scratch"
 DISTRIB_RELEASE="$version"
 DISTRIB_DESCRIPTION="Linux From Scratch"
