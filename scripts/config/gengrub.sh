e0b114d1a176927f1ab39f08e8ec0c8890d1a211
Hendri Pretorius <pretorh@gmail.com>
2024-08-10 15:18:15 +0200

    boot: change grub generation script to run dynamically
    change the script to generate grub menu entries each time, instead of
    generating the config statically once
    
    add a single root `menuentry` using the `/dev/` root, and move all other
    entries as a `submenu` item. change the labeling of the menu items to be
    more LFS- and drive-specific
    
    update steps to match this usage

--- scripts/config/gengrub.sh
+++ scripts/config/gengrub.sh
@@ -1,24 +1,65 @@
 #!/usr/bin/env bash
+set -e
 
-ROOT="$(findmnt / -o SOURCE,UUID | tail -n1)"
-IFS=" " read -r -a sources <<< "$ROOT"
+root_columns="$(findmnt --target "$0" -o SOURCE,UUID,PARTLABEL | tail -n1)"
+IFS=" " read -r -a script_roots <<< "$root_columns"
+root_dev=${script_roots[0]}
+root_uuid=${script_roots[1]}
+root_label=${script_roots[2]}
+if [ -z "$root_label" ] ; then root_label=$root_uuid ; fi
 
-echo "#!/bin/sh"
-echo "exec tail -n +3 \$0"
-echo ""
-echo "# begin of generated menuentries for lfs named kernels"
+is_first=1
 
-while IFS= read -r -d '' file
-do
-    base_name=${file//\/boot\/}
+output() {
+    echo "[LFS-grub] $*" >&2
+}
+
+add_menuentry() {
+    root_mode=$1
+    base_name=$2
+
+    root=$root_dev
+    if [ "$root_mode" == "uuid" ] ; then
+        root="UUID=$root_uuid"
+    fi
+
     label="${base_name:8}"
-    label=${label//lfs-/LFS: }
+    label=${label//lfs-/}
 
-    echo "menuentry \"$label\" {"
-    echo "    load_video"
-    echo "    # needs initramfs: linux /$base_name root=UUID=${sources[1]} ro"
-    echo "    linux /$base_name root=${sources[0]} ro"
+    indent="    "
+    if [ "$is_first" ] ; then
+        indent=""
+        label="LFS on $root with $label"
+    elif [ "$root_mode" == "uuid" ] ; then
+        label="$label (UUID)"
+    fi
+
+    echo "${indent}menuentry '$label' {"
+    echo "${indent}    load_video"
+    echo "${indent}    linux /$base_name root=$root ro"
+    echo "${indent}}"
+}
+
+output "generating menu entries for $root_label"
+while read -r file; do
+    output "found vmlinuz $file"
+    base_name=${file//\/boot\/}
+
+    if [ "$is_first" ] ; then
+        add_menuentry "dev" "$base_name"
+        is_first=
+        echo ""
+        echo "submenu 'LFS on $root_label' {"
+    else
+        add_menuentry "dev" "$base_name"
+    fi
+
+    add_menuentry "uuid" "$base_name"
+done <   <(ls -1 /boot/vmlinuz-*lfs* --sort=time)
+
+if [ "$is_first" ] ; then
+    output "no vmlinuz-*lfs* kernels found!"
+    echo "# no LFS kernels found"
+else
     echo "}"
-done <   <(find /boot -type f -name 'vmlinuz-*lfs*' -print0)
-
-echo "# end of generated menuentries for lfs named kernels"
+fi
